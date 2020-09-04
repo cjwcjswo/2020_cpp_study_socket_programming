eb75a5a3d6cb95ba06c503c91d0363edb13ea41a
최진우 <cjwoov@com2us.com>
2020-09-04 19:18:10 +0900

    채팅 클라이언트 뼈대 추가

---
+++ AsyncIOGameServer/ChatClient/PacketProcessForm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_test_client
+{
+    public partial class mainForm
+    {
+        void AddEchoMessageList(string message)
+        {
+            if (chatListBox.Items.Count > 512)
+            {
+                chatListBox.Items.Clear();
+            }
+
+            chatListBox.Items.Add(message);
+            chatListBox.SelectedIndex = chatListBox.Items.Count - 1;
+        }
+    }
+}
---
+++ AsyncIOGameServer/ChatClient/mainForm.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace csharp_test_client
+{
+    public partial class mainForm : Form
+    {
+        ClientSimpleTcp Network = new ClientSimpleTcp();
+
+        bool IsNetworkThreadRunning = false;
+        bool IsBackGroundProcessRunning = false;
+
+        System.Threading.Thread NetworkReadThread = null;
+        System.Threading.Thread NetworkSendThread = null;
+
+        Queue<byte[]> RecvPacketQueue = new Queue<byte[]>();
+        Queue<byte[]> SendPacketQueue = new Queue<byte[]>();
+
+        System.Windows.Threading.DispatcherTimer dispatcherUITimer;
+
+
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            IsNetworkThreadRunning = true;
+            NetworkReadThread = new System.Threading.Thread(this.NetworkReadProcess);
+            NetworkReadThread.Start();
+            NetworkSendThread = new System.Threading.Thread(this.NetworkSendProcess);
+            NetworkSendThread.Start();
+
+            IsBackGroundProcessRunning = true;
+            dispatcherUITimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherUITimer.Tick += new EventHandler(BackGroundProcess);
+            dispatcherUITimer.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            dispatcherUITimer.Start();
+
+            btnDisconnect.Enabled = false;
+
+            DevLog.Write("프로그램 시작 !!!", LOG_LEVEL.INFO);
+        }
+
+        private void mainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            IsNetworkThreadRunning = false;
+            IsBackGroundProcessRunning = false;
+
+            Network.Close();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            string address = textBoxIP.Text;
+
+            if (checkBoxLocalHostIP.Checked)
+            {
+                address = "127.0.0.1";
+            }
+
+            int port = Convert.ToInt32(textBoxPort.Text);
+
+            if (Network.Connect(address, port))
+            {
+                labelStatus.Text = string.Format("{0}. 서버에 접속 중", DateTime.Now);
+                btnConnect.Enabled = false;
+                btnDisconnect.Enabled = true;
+
+                DevLog.Write($"서버에 접속 중", LOG_LEVEL.INFO);
+            }
+            else
+            {
+                labelStatus.Text = string.Format("{0}. 서버에 접속 실패", DateTime.Now);
+            }
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            SetDisconnect();
+            Network.Close();
+        }
+
+        void NetworkReadProcess()
+        {
+            while (IsNetworkThreadRunning)
+            {
+                if (Network.IsConnected() == false)
+                {
+                    System.Threading.Thread.Sleep(1);
+                    continue;
+                }
+
+                var recvData = Network.Receive();
+
+                if (recvData != null)
+                {
+                    if(recvData.Item1 > 0)
+                    {
+                        lock (((System.Collections.ICollection)RecvPacketQueue).SyncRoot)
+                        {
+                            RecvPacketQueue.Enqueue(recvData.Item2);
+                        }
+                    }
+                    //DevLog.Write($"받은 데이터: {recvData.Item2}", LOG_LEVEL.INFO);
+                }
+                else
+                {
+                    Network.Close();
+                    SetDisconnect();
+                    DevLog.Write("서버와 접속 종료 !!!", LOG_LEVEL.INFO);
+                }
+            }
+        }
+
+        void NetworkSendProcess()
+        {
+            while (IsNetworkThreadRunning)
+            {
+                System.Threading.Thread.Sleep(1);
+
+                if (Network.IsConnected() == false)
+                {
+                    continue;
+                }
+
+                lock (((System.Collections.ICollection)SendPacketQueue).SyncRoot)
+                {
+                    if (SendPacketQueue.Count > 0)
+                    {
+                        var packet = SendPacketQueue.Dequeue();
+                        Network.Send(packet);
+                    }
+                }
+            }
+        }
+
+
+        void BackGroundProcess(object sender, EventArgs e)
+        {
+            ProcessLog();
+
+            try
+            {
+                byte[] data;
+
+                lock (((System.Collections.ICollection)RecvPacketQueue).SyncRoot)
+                {
+                    if (RecvPacketQueue.Count() > 0)
+                    {
+                        data = RecvPacketQueue.Dequeue();
+                        Int16 packetSize = BitConverter.ToInt16(data, 0);
+                        byte[] bodyData = new byte[packetSize - 4];
+                        Buffer.BlockCopy(data, 4, bodyData, 0, packetSize - 4);
+                        AddEchoMessageList(Encoding.UTF8.GetString(bodyData));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(string.Format("ReadPacketQueueProcess. error:{0}", ex.Message));
+            }
+        }
+
+        private void ProcessLog()
+        {
+            // 너무 이 작업만 할 수 없으므로 일정 작업 이상을 하면 일단 패스한다.
+            int logWorkCount = 0;
+
+            while (IsBackGroundProcessRunning)
+            {
+                System.Threading.Thread.Sleep(1);
+
+                string msg;
+
+                if (DevLog.GetLog(out msg))
+                {
+                    ++logWorkCount;
+
+                    if (listBoxLog.Items.Count > 512)
+                    {
+                        listBoxLog.Items.Clear();
+                    }
+
+                    listBoxLog.Items.Add(msg);
+                    listBoxLog.SelectedIndex = listBoxLog.Items.Count - 1;
+                }
+                else
+                {
+                    break;
+                }
+
+                if (logWorkCount > 8)
+                {
+                    break;
+                }
+            }
+        }
+
+        private delegate void DisconnectDelegate();
+        public void SetDisconnect()
+        {
+            if(chatListBox.InvokeRequired)
+            {
+                var d = new DisconnectDelegate(SetDisconnect);
+                Invoke(d);
+            }
+            else
+            {
+                chatListBox.Items.Clear();
+                if (btnConnect.Enabled == false)
+                {
+                    btnConnect.Enabled = true;
+                    btnDisconnect.Enabled = false;
+                }
+                labelStatus.Text = "서버 접속이 끊어짐";
+            }
+            SendPacketQueue.Clear();
+        }
+    }
+}
